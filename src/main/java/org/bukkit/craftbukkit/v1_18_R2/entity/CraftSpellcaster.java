1a5932d01dd7908f77107f00b2365319aa8bfca1
Kotori0629 <79943751@qq.com>
2024-12-06 23:35:09 +0800

    feat: add illager spell registries

--- src/main/java/org/bukkit/craftbukkit/v1_18_R2/entity/CraftSpellcaster.java
+++ src/main/java/org/bukkit/craftbukkit/v1_18_R2/entity/CraftSpellcaster.java
@@ -1,9 +1,14 @@
 package org.bukkit.craftbukkit.v1_18_R2.entity;
 
+import catserver.server.CatServer;
+import catserver.server.utils.EnumHelper;
 import com.google.common.base.Preconditions;
 import net.minecraft.world.entity.monster.SpellcasterIllager;
 import org.bukkit.craftbukkit.v1_18_R2.CraftServer;
 import org.bukkit.entity.Spellcaster;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CraftSpellcaster extends CraftIllager implements Spellcaster {
 
@@ -33,9 +38,24 @@ public class CraftSpellcaster extends CraftIllager implements Spellcaster {
         getHandle().setIsCastingSpell(toNMSSpell(spell));
     }
 
+    // CatServer start
     public static Spell toBukkitSpell(SpellcasterIllager.IllagerSpell spell) {
-        return Spell.valueOf(spell.name());
+        try {
+            return Spellcaster.Spell.valueOf(spell.name());
+        } catch (IllegalArgumentException e) {
+            var newTypes = new ArrayList<Spell>();
+            var forgeCount = SpellcasterIllager.IllagerSpell.values().length;
+            for (var id = Spellcaster.Spell.values().length; id < forgeCount; id++) {
+                var name = SpellcasterIllager.IllagerSpell.values()[id].name();
+                var newPhase = EnumHelper.makeEnum(Spellcaster.Spell.class, name, id, List.of(), List.of());
+                newTypes.add(newPhase);
+                CatServer.LOGGER.debug("Save-IllagerSpell: {} - {}", name, newPhase);
+            }
+            EnumHelper.addEnums(Spellcaster.Spell.class, newTypes);
+            return toBukkitSpell(spell);
+        }
     }
+    // CatServer end
 
     public static SpellcasterIllager.IllagerSpell toNMSSpell(Spell spell) {
         return SpellcasterIllager.IllagerSpell.byId(spell.ordinal());
